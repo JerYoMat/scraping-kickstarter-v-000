b6718621a845cb037735b00261c94c238122ffc7
Jeremy Emata <jeremy.emata@gmail.com>
2018-10-10 01:38:37 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,4 +11,6 @@ def create_project_hash
   binding.pry
 end
 
-# projects: kickstarter.css("li.project.grid_4")
+# project: kickstarter.css("li.project.grid_4")
+#title: project.css("h2.bbcard_name strong a")[0].text 
+# 
